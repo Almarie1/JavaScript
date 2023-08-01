44f69fd0fec5731d86826e7ff31c5f3940b9ef87
Almarie Mouton <almariemouton13@gmail.com>
2023-08-01 11:33:51 +0200

    latest

--- counter_app/index.js
+++ counter_app/index.js
@@ -1,3 +1,11 @@
 //initialise the count as 0
 
 let count = 0;
+let countEL = document.getElementById("count-el");
+let saveEL = document.getElementById("seve-el");
+
+function increment() {
+    //console.log("clicked")
+    count++
+    countEL.textContent = count
+}
