23832d2d4c1215a47f12c8a513ed38edd2a97878
Almarie Mouton <almariemouton13@gmail.com>
2023-08-15 10:01:58 +0200

    js added

--- modal_menu_slider/scripts.js
+++ modal_menu_slider/scripts.js
@@ -5,3 +5,11 @@ const modal = document.getElementById('modal');
 
 //toggle nav
 toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'));
+
+//show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+//hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
