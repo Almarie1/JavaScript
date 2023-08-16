a1011c2f8b6713b44cf101bf3eacd9fa7af9c6a0
Almarie Mouton <almariemouton13@gmail.com>
2023-08-16 09:28:38 +0200

    addTask function

--- task_list/app.js
+++ task_list/app.js
@@ -19,4 +19,18 @@ function addTask(e) {
     }
     //create li element
     const li = document.createElement('li');
+    //add class - add meterialize class
+    li.className = 'collection-item';
+    //create a text note and append to li
+    li.appendChild(document.createTextNode(taskInput.value));\
+    //create new link element
+    const link = document.createElement('a');
+    //add class
+    link.className = 'delete-item-secondary-content';
+    //add icon html
+    link.innerHTML = '<i class ="fa fa-remove"></i>'; //create an x mark icon
+    //append the link to li
+    li.appendChild(link);
+    taskList.appendChild(li);
+
 }
