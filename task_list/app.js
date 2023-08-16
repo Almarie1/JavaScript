52d43805a134528a703f1b9a2e0f5c8bc356c31c
Almarie Mouton <almariemouton13@gmail.com>
2023-08-16 10:39:03 +0200

    redid apps.js

--- task_list/app.js
+++ task_list/app.js
@@ -1,51 +1,61 @@
-//define UI variables
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-task');
-const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task');
+// define UI variables
+const form = document.querySelector('#task-form')
+const taskList = document.querySelector('.collection')
+const clearbtn = document.querySelector('.clear-tasks')
+const filter = document.querySelector('#filter')
+const taskInput = document.querySelector('#task')
 
-//load all eventy listeners
-loadEventListeners();
 
+// Load all event listeners (2)
+loadEventListeners()
+
+ 
 function loadEventListeners() {
-    //add task event
-    form.addEventListener('submit', addTask);
-    taskList.addEventListener('click', removeTask);
+    // add task event
+    form.addEventListener('submit', addTask)
+    taskList.addEventListener('click', removeTask)
+    clearbtn.addEventListener('click', clearTasks)
 }
+
+ 
 
 function addTask(e) {
-    if(taskInput.value === '') {
-        alert('Add a task');
+    if(taskInput.value === '') {alert('Add a task')
     }
-    //create li element
-    const li = document.createElement('li');
-    //add class - add meterialize class
-    li.className = 'collection-item';
-    //create a text note and append to li
-    li.appendChild(document.createTextNode(taskInput.value));
-    //create new link element
-    const link = document.createElement('a');
-    //add class
-    link.className = 'delete-item-secondary-content';
-    //add icon html
-    link.innerHTML = '<i class ="fa fa-remove"></i>'; //create an x mark icon
-    //append the link to li
+    // create li
+    const li = document.createElement('li')
+    // add class
+    li.className = 'collection-item'
+    // // create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value))
+    const link = document.createElement('a')
+    // // Add class
+    link.className = 'delete-item secondary-content'
+    // // Add HTML
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // append link to li
     li.appendChild(link);
-    taskList.appendChild(li);
+    taskList.appendChild(li)
 
-    //clear input
+    // Clear Tasks
     taskInput.value = '';
-    e.preventDefault();
-
+    e.preventDefault()
 }
 
-//remove task
+ 
+
 function removeTask(e) {
-    if(e.target.parentElement.classList.contains('delete-item')) {
-        console.log(e.target);
-        if(confirm('Are you sure you want to clear the list?')) {
-            e.target.parentElement.parentElement.remove();
+    if(e.target.parentElement.classList.contains('delete-item')){
+        console.log(e.target)
+        if(confirm('Are you sure lol?')){
+            e.target.parentElement.parentElement.remove()
         }
+    }
+}
+
+ 
+function clearTasks() {
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild)
     }
 }
