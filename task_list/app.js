737dc167a2c9df465e48cc93c66be9c0445f5de4
Almarie Mouton <almariemouton13@gmail.com>
2023-08-16 10:57:01 +0200

    filter

--- task_list/app.js
+++ task_list/app.js
@@ -18,7 +18,7 @@ function loadEventListeners() {
     //clear task
     clearbtn.addEventListener('click', clearTasks);
     //filter task event
-    filter.addEventListener(keyup);
+    filter.addEventListener('keyup',filterTasks);
 }
  
 
@@ -65,4 +65,13 @@ function clearTasks() {
 //filter tasks
 function filterTasks(e) {
     const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+    console.log(allItems);
 }
